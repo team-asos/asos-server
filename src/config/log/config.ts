b516027429945e8e26f48a5350637fd67dddebbe
Kevin Kim <swputer@gmail.com>
2021-09-17 23:45:12 +0900

    chore: config 파일 구조 변경

---
+++ src/config/log/config.ts
@@ -0,0 +1,53 @@
+export const config = {
+  pinoHttp: {
+    wrapSerializers: false,
+    serializers: {
+      req(req) {
+        return {
+          method: req.method,
+          url: req.url,
+          query: req.query,
+          params: req.params,
+          headers: {
+            host: req.headers.host,
+            userAgent: req.headers['user-agent'],
+          },
+          remoteAddress: `${
+            req.headers['x-forwarded-for'] || req.socket.remoteAddress
+          }`,
+        };
+      },
+      res(res) {
+        return {
+          status: res.statusCode,
+        };
+      },
+      responseTime(responseTime) {
+        return `${responseTime}ms`;
+      },
+    },
+    customLogLevel: function (res, err) {
+      if (res.statusCode >= 400 && res.statusCode < 500) {
+        return 'warn';
+      } else if (res.statusCode >= 500 || err) {
+        return 'error';
+      }
+      return 'info';
+    },
+    customSuccessMessage: function (res) {
+      if (res.statusCode === 404) {
+        return 'resource not found';
+      }
+      return 'request completed';
+    },
+    customErrorMessage: function (error, res) {
+      return 'request errored with status code: ' + res.statusCode;
+    },
+    prettyPrint: {
+      colorize: true,
+      levelFirst: true,
+      translateTime: 'SYS:standard',
+      ignore: 'pid,hostname',
+    },
+  },
+};
