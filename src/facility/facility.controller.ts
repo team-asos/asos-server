ca8190ac33e12ce8bc7e467c6fc3af1cc9e87f81
yunbeom <uiurihappy@sju.ac.kr>
2021-11-04 16:59:28 +0900

    Feat: 시설 API CRUD 기능 추가

--- src/facility/facility.controller.ts
+++ src/facility/facility.controller.ts
@@ -5,11 +5,13 @@ import {
   Get,
   HttpCode,
   Param,
+  Patch,
   Post,
 } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { CreateFacilityDto } from './dtos/create-facility.dto';
+import { UpdateFacilityDto } from './dtos/update-facility.dto';
 import { Facility } from './facility.entity';
 import { FacilityService } from './facility.service';
 
@@ -18,6 +20,7 @@ import { FacilityService } from './facility.service';
 export class FacilityController {
   constructor(private readonly facilityService: FacilityService) {}
 
+  //시설 조회
   @Get()
   @HttpCode(200)
   @ApiOperation({ summary: '모든 시설 조회' })
@@ -28,6 +31,7 @@ export class FacilityController {
     return facilities;
   }
 
+  //시설 생성
   @Post()
   @HttpCode(201)
   @ApiOperation({ summary: '시설 생성' })
@@ -41,9 +45,23 @@ export class FacilityController {
     return 'success';
   }
 
+  //시설 수정
+  @Patch(':facilityId')
+  @HttpCode(200)
+  @ApiOperation({ summary: '특정 시설 수정' })
+  @ApiResponse({ status: 200, description: 'Success' })
+  async updateOne(
+    @Param('facilityId') facilityId: number,
+    @Body() updatefacilityDto: UpdateFacilityDto,
+  ): Promise<string> {
+    await this.facilityService.updateOne(facilityId, updatefacilityDto);
+    return 'success';
+  }
+
+  //시설 삭제
   @Delete(':facilityId')
   @HttpCode(200)
-  @ApiOperation({ summary: '시설 삭제' })
+  @ApiOperation({ summary: '특정 시설 삭제' })
   @ApiResponse({ status: 200, description: 'Success' })
   async deleteOne(@Param('facilityId') facilityId: number): Promise<string> {
     await this.facilityService.deleteOne(facilityId);
