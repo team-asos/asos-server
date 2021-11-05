f69ec8139ceceddba853a525182b9532af99df88
Dohyeon <swputer@gmail.com>
2021-11-05 17:49:56 +0900

    feat: JWT로 본인 정보 조회 API 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,13 @@
 import * as bcrypt from 'bcrypt';
 import HttpError from 'src/common/exceptions/http.exception';
 import { ErrorMessage } from 'src/common/utils/errors/ErrorMessage';
-import { LoginUserDto } from 'src/user/dtos/login-user.dto';
 import { User } from 'src/user/user.entity';
 import { UserRepository } from 'src/user/user.repository';
 
 import { HttpStatus, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+
+import { LoginUserDto } from './dtos/login-user.dto';
 
 @Injectable()
 export class AuthService {
