f56405381cb08b6735ff48d3d2cfd52751b878f0
Dohyeon <swputer@gmail.com>
2021-11-06 04:33:07 +0900

    docs: App Controller Swagger에서 제외

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, HttpCode } from '@nestjs/common';
+import { ApiExcludeController } from '@nestjs/swagger';
 
+@ApiExcludeController()
 @Controller()
 export class AppController {
   @Get()
