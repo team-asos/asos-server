90f7762cbf76d87d18d4f9804591789f384d6a2d
Dohyeon <swputer@gmail.com>
2021-11-25 02:14:19 +0900

    feat: ESL converter 예외처리 및 테스트

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,10 +1,11 @@
 import { createObjectCsvWriter } from 'csv-writer';
+import * as fs from 'fs';
 import * as moment from 'moment';
 import { FtpService } from 'nestjs-ftp';
 import { ReservationRepository } from 'src/reservation/reservation.repository';
 
 import { Injectable } from '@nestjs/common';
-import { Cron } from '@nestjs/schedule';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class TaskService {
@@ -13,31 +14,28 @@ export class TaskService {
     private readonly ftpService: FtpService,
   ) {}
 
-  @Cron('0 */3 * * * *')
+  @Cron(CronExpression.EVERY_5_SECONDS)
   async updateReservationStatus(): Promise<void> {
-    await this.reservationRepository.updateReservationStatus();
+    await this.reservationRepository.updateStatus();
 
-    // const file = await this.parseReservation();
-
-    // try {
-    //   await this.ftpService.upload(`./file/${file}`, `Import/${file}`);
-    // } catch (error) {
-    //   console.log(error);
-    // }
+    await this.eslConverter();
 
     return;
   }
 
-  private async parseReservation(): Promise<string> {
+  private async eslConverter(): Promise<void> {
     const reservations = await this.reservationRepository.parseReservation();
 
     const parse = reservation => {
       return {
+        floor: reservation.seat
+          ? `${reservation.seat.floor.name}`
+          : `${reservation.room.floor.name}`,
         location: reservation.seat
-          ? `${reservation.seat.floor.name}-${reservation.seat.name}`
-          : `${reservation.room.floor.name}-${reservation.room.name}`,
-        start_time: moment(reservation.startTime).format('HH : MM'),
-        end_time: moment(reservation.endTime).format('HH : MM'),
+          ? `${reservation.seat.name}`
+          : `${reservation.room.name}`,
+        start_time: moment(reservation.startTime).format('HH : mm'),
+        end_time: moment(reservation.endTime).format('HH : mm'),
         department: reservation.user.department,
         phone: reservation.user.tel,
         name: reservation.user.name,
@@ -50,13 +48,14 @@ export class TaskService {
 
     const records = reservations.map(reservation => parse(reservation));
 
-    const path = './file/';
-    const fileName = `import_${moment().format('YYYYMMDDHHmmss')}`;
-    const extension = '.csv';
+    const originPath = './file/';
+    const destinationPath = 'Import/';
+    const file = `import_${moment().format('YYYYMMDDHHmmss')}.csv`;
 
     const csvWriter = createObjectCsvWriter({
-      path: path + fileName + extension,
+      path: originPath + file,
       header: [
+        { id: 'floor', title: 'floor' },
         { id: 'location', title: 'location' },
         { id: 'start_time', title: 'start_time' },
         { id: 'end_time', title: 'end_time' },
@@ -70,6 +69,23 @@ export class TaskService {
 
     await csvWriter.writeRecords(records);
 
-    return fileName + extension;
+    fs.unlinkSync(`${originPath}${file}`);
+
+    // try {
+    //   const response = await this.ftpService.upload(
+    //     `${originPath}${file}`,
+    //     `${destinationPath}${file}`,
+    //   );
+
+    //   if (response.code === 226) console.log('FTP 전송 성공!');
+    //   else if (response.code === 0) console.log('FTP 전송 실패!');
+    //   else console.log('알 수 없는 응답');
+    // } catch (error) {
+    //   console.log(error);
+    // } finally {
+    //   fs.unlinkSync(`${originPath}${file}`);
+    // }
+
+    return;
   }
 }
