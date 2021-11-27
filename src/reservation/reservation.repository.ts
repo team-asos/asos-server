fe984975e13f4c0bb34c5dab3629390380f0c563
Dohyeon <swputer@gmail.com>
2021-11-27 17:10:26 +0900

    build: 병합 시 발생한 충돌 해결

--- src/reservation/reservation.repository.ts
+++ src/reservation/reservation.repository.ts
@@ -92,31 +92,4 @@ export class ReservationRepository extends Repository<Reservation> {
 
     return;
   }
-
-  async parseReservation(): Promise<any> {
-    const reservations = await this.createQueryBuilder('reservation')
-      .leftJoinAndSelect('reservation.user', 'user')
-      .leftJoinAndSelect('reservation.seat', 'seat')
-      .leftJoinAndSelect('reservation.room', 'room')
-      .leftJoinAndSelect('seat.floor', 'seat.floor')
-      .leftJoinAndSelect('room.floor', 'room.floor')
-      .select([
-        'reservation.startTime',
-        'reservation.endTime',
-        'reservation.status',
-        'user.name',
-        'user.tel',
-        'user.department',
-        'seat.name',
-        'seat.tagId',
-        'seat.floor.name',
-        'room.name',
-        'room.tagId',
-        'room.floor.name',
-      ])
-      .where('reservation.status = 1')
-      .getMany();
-
-    return reservations;
-  }
 }
