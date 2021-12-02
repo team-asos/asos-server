d1ea61d5c751e82a0e0256c36cb1f7c59ff22615
Dohyeon <swputer@gmail.com>
2021-12-02 22:47:37 +0900

    feat: 회의실 더미데이터 추가 및 무작위 부서, 직책 생성

--- src/mock/mock.module.ts
+++ src/mock/mock.module.ts
@@ -1,12 +1,13 @@
+import { FloorModule } from 'src/api/floor/floor.module';
+import { RoomModule } from 'src/api/room/room.module';
+import { SeatModule } from 'src/api/seat/seat.module';
 import { UserModule } from 'src/api/user/user.module';
 import { MockService } from 'src/mock/mock.service';
 
 import { Module } from '@nestjs/common';
-import { FloorModule } from 'src/api/floor/floor.module';
-import { SeatModule } from 'src/api/seat/seat.module';
 
 @Module({
-  imports: [UserModule, FloorModule, SeatModule],
+  imports: [UserModule, FloorModule, SeatModule, RoomModule],
   providers: [MockService],
   exports: [MockService],
 })
--- src/mock/mock.service.ts
+++ src/mock/mock.service.ts
@@ -1,14 +1,16 @@
 import * as faker from 'faker/locale/ko';
 import { CreateFloorDto } from 'src/api/floor/dtos/create-floor.dto';
 import { FloorService } from 'src/api/floor/floor.service';
+import { CreateRoomDto } from 'src/api/room/dtos/create-room.dto';
+import { RoomService } from 'src/api/room/room.service';
 import { CreateSeatDto } from 'src/api/seat/dtos/create-seat.dto';
 import { SeatService } from 'src/api/seat/seat.service';
 import { CreateUserDto } from 'src/api/user/dtos/create-user.dto';
 import { UserService } from 'src/api/user/user.service';
+import { ConfigService } from 'src/config/config.service';
 import { getConnection } from 'typeorm';
 
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from 'src/config/config.service';
 
 @Injectable()
 export class MockService {
@@ -17,7 +19,61 @@ export class MockService {
     private readonly userService: UserService,
     private readonly floorService: FloorService,
     private readonly seatService: SeatService,
+    private readonly roomService: RoomService,
   ) {}
+
+  getBetween(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  getDepartment(): string {
+    const DEPARTMENTS = [
+      '사장실',
+      '비서실',
+      '기획실',
+      '인사부',
+      '인력개발부',
+      '재무부',
+      '총무부',
+      '경리부',
+      '국내영업부',
+      '해외영업부',
+      '해외사업부',
+      '영업관리부',
+      '자재부',
+      '구매부',
+      '생산관리부',
+      '내부품질관리부',
+      '외부품질관리부',
+      '고객지원부',
+      '연구개발부',
+      '홍보부',
+      '물류부',
+    ];
+
+    return DEPARTMENTS[this.getBetween(0, DEPARTMENTS.length - 1)];
+  }
+
+  getPosition(): string {
+    const POSITIONS = [
+      '회장',
+      '사장',
+      '부사장',
+      '전무이사',
+      '상무이사',
+      '이사',
+      '부장',
+      '차장',
+      '과장',
+      '대리',
+      '계장',
+      '주임',
+      '사원',
+      '인턴',
+    ];
+
+    return POSITIONS[this.getBetween(0, POSITIONS.length - 1)];
+  }
 
   async dummy(): Promise<void> {
     /**
@@ -39,15 +95,18 @@ export class MockService {
 
     // FLOOR SETTING
     const FLOOR_COUNT = this.configService.getNumber('FLOOR_COUNT') || 7;
-    const FLOOR_WIDTH = this.configService.getNumber('FLOOR_WIDTH') || 24;
-    const FLOOR_HEIGHT = this.configService.getNumber('FLOOR_HEIGHT') || 24;
+    const FLOOR_WIDTH = this.configService.getNumber('FLOOR_WIDTH') || 16;
+    const FLOOR_HEIGHT = this.configService.getNumber('FLOOR_HEIGHT') || 16;
 
     // SEAT SETTING
-    const SEAT_COUNT = this.configService.getNumber('SEAT_COUNT') || 30;
+    const SEAT_COUNT = this.configService.getNumber('SEAT_COUNT') || 200;
     const SEAT_TAG_START =
       this.configService.getNumber('SEAT_TAG_START') || 10000;
 
     // ROOM SETTING
+    const ROOM_COUNT = this.configService.getNumber('ROOM_COUNT') || 10;
+    const ROOM_WIDTH = this.configService.getNumber('ROOM_WIDTH') || 3;
+    const ROOM_HEIGHT = this.configService.getNumber('ROOM_HEIGHT') || 4;
     const ROOM_TAG_START =
       this.configService.getNumber('ROOM_TAG_START') || 20000;
 
@@ -68,6 +127,7 @@ export class MockService {
     let users: CreateUserDto[] = [];
     let floors: CreateFloorDto[] = [];
     let seats: CreateSeatDto[] = [];
+    let rooms: CreateRoomDto[] = [];
 
     /**
      * 더미데이터 생성
@@ -80,8 +140,8 @@ export class MockService {
         tel: faker.phone.phoneNumber('010-####-####'),
         role: 0,
         employeeId: faker.finance.routingNumber(),
-        department: faker.name.jobArea(),
-        position: faker.name.jobTitle(),
+        department: this.getDepartment(),
+        position: this.getPosition(),
       };
     });
 
@@ -94,15 +154,30 @@ export class MockService {
     });
 
     seats = Array.from({ length: SEAT_COUNT }, (_, i) => {
-      const floorId = faker.datatype.number(FLOOR_COUNT - 1);
+      const floorId = this.getBetween(1, FLOOR_COUNT);
 
       return {
         name: `${String.fromCharCode(65 + ((floorId - 1) % 26))}${i + 1}`,
-        x: faker.datatype.number(FLOOR_WIDTH),
-        y: faker.datatype.number(FLOOR_HEIGHT),
+        x: this.getBetween(1, FLOOR_WIDTH - 2),
+        y: this.getBetween(1, FLOOR_HEIGHT - ROOM_HEIGHT - 4),
         width: 1,
         height: 1,
         tagId: SEAT_TAG_START + i,
+        floorId,
+      };
+    });
+
+    rooms = Array.from({ length: ROOM_COUNT }, (_, i) => {
+      const floorId = this.getBetween(1, FLOOR_COUNT);
+
+      return {
+        name: `회의실${i + 1}`,
+        maxUser: 4,
+        x: this.getBetween(1, FLOOR_WIDTH - ROOM_WIDTH - 2),
+        y: FLOOR_HEIGHT - ROOM_HEIGHT - 2,
+        width: ROOM_WIDTH,
+        height: ROOM_HEIGHT,
+        tagId: ROOM_TAG_START + i,
         floorId,
       };
     });
@@ -118,6 +193,10 @@ export class MockService {
 
       await Promise.all(
         seats.map(async seat => await this.seatService.createOne(seat)),
+      );
+
+      await Promise.all(
+        rooms.map(async room => await this.roomService.createOne(room)),
       );
     } catch (error) {
       console.log(`error: ${error}`);
